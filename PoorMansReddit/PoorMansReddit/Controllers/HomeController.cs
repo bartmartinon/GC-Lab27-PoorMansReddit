b2ca083c5409e2bc239dd845efa32c33d3fa6e4a
Bart Martinon <barthelemy.martinon@gmail.com>
2020-11-23 16:59:34 -0500

    Added VS2019 project. Got base requirements fulfilled

---
+++ PoorMansReddit/PoorMansReddit/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PoorMansReddit.Models;
+
+namespace PoorMansReddit.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public RedditDAL Context = new RedditDAL();
+
+        public IActionResult Index()
+        {
+            string output = Context.CallAPI();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult RedditPost()
+        {
+            List<Child> childElements = Context.ConvertToChildElements();
+            return View(childElements);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PoorMansReddit/PoorMansReddit/Models/RedditDAL.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PoorMansReddit.Models
+{
+    public class RedditDAL
+    {
+        public string CallAPI()
+        {
+            string endpoint = "https://www.reddit.com/r/aww/.json";
+            // Extra setup tends to go around here (Additional headers, passwords, endpoint shenanigans)
+
+            // Sending a letter out to someone, hoping for a response
+            HttpWebRequest request = WebRequest.CreateHttp(endpoint);
+
+            // Pulls in the response. If it worked, we get a file containing our JSON.
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Store the JSON as a string into File I/O
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string output = rd.ReadToEnd();
+            return output;
+        }
+
+        public List<Child> ConvertToChildElements()
+        {
+            string data = CallAPI();
+            Rootobject r = JsonConvert.DeserializeObject<Rootobject>(data);
+            Data d = r.data;
+            List<Child> childElements = new List<Child>();
+            for (int i = 0; i < d.children.Length; i++)
+            {
+                Child child = d.children[i];
+                childElements.Add(child);
+            }
+            return childElements;
+        }
+    }
+}
